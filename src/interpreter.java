6af7f337e303360cabe62c95da33154561f60fd7
Todd Broll <todd.broll@gmail.com>
2016-04-19 20:38:33 -0500

    initialized the java program I am about to create from the Fortranz program

--- src/interpreter.java
+++ src/interpreter.java
@@ -1,13 +1,40 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
 public class Interpreter{
-    private final String newJavaFile = "RNG.java";
+    private String[] operators = {"+", "-", "/", "*"};
+    private String[] nativeWords = {"WRITE", "READ", "IF", "THEN", "ELSE",
+        "CALL", "END IF", "END","SUBROUTINE", "DO","CONTINUE", "GOTO"}; 
     public static void main(String [] args){
-        File file = new File("RNG.f");
+        Scanner scanner = new Scanner(System.in); 
+        System.out.println("Which fortranz program do you want me to convert to Java?");
+        String file = scanner.next();
         Interpreter interpreter = new Interpreter();
-        interpreter.start(file);
+        interpreter.readInput(file);
     }
-    private void start(File inputfile){
-        readInput(inputfile);
-        convertInputfileToJava();
-        writeOutputFile();
+    private void readInput(String input){
+        initialize(input);
+        String thisLine = null;
+        try{
+        BufferedReader br = new BufferedReader(new FileReader(input));
+        while((thisLine = br.readLine()) != null){
+        convertInputfileToJava(thisLine);
     }
+        }
+        catch(IOException e){
+            System.out.println("error: " + e.getMessage());
+        }
+    }
+    private void convertInputfileToJava(String currentLine){
+    }
+    private void writeOutputFile(){
+    }
+    private void initialize(String input){
+        String[] filename = input.split("\\.");
+        System.out.println(filename.length);
+        input = filename[0];
+        System.out.println("import java.util.*; \npublic class "+input+" {");
+        System.out.println("public static void main(String[] args){");
+    }
+}
 
